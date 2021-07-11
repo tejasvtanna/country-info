b87ec3ac5daf722f285bed7b9e10971b0ab1e8c7
tejasvtanna <56431505+tejasvtanna@users.noreply.github.com>
2021-07-11 07:38:33 +0530

    Update CountryTable.js

--- src/components/CountryTable.js
+++ src/components/CountryTable.js
@@ -3,61 +3,55 @@ import axios from 'axios'
 import { DataGrid } from '@material-ui/data-grid'
 
 const CountryTable = () => {
-    const [allCountryData, setAllCountryData] = useState([])
-    const [filteredData, setFilteredData] = useState([])
-    const [countryFilter, setCountryFilter] = useState('')
+  const [allCountryData, setAllCountryData] = useState([])
+  const [filteredData, setFilteredData] = useState([])
+  const [countryFilter, setCountryFilter] = useState('')
 
-    const columns = [
-        { field: 'name', headerName: 'Name', width: 130 },
-        { field: 'capital', headerName: 'Capital', width: 130 },
-        { field: 'region', headerName: 'Region', width: 130 },
-        { field: 'domains', headerName: 'Top Level Domains', width: 200 },
-        { field: 'borders', headerName: 'Borders', width: 200 },
-    ]
-    useEffect(() => {
-        const fetchCountryData = async () => {
-            const { data } = await axios.get(
-                'https://restcountries.eu/rest/v2/all'
-            )
-            const dataRequired = data.map((obj, idx) => ({
-                id: idx,
-                name: obj.name,
-                capital: obj.capital,
-                region: obj.region,
-                domains: obj.topLevelDomain.join(', '),
-                borders: obj.borders.join(', '),
-            }))
-            setAllCountryData(dataRequired)
-        }
+  const columns = [
+    { field: 'name', headerName: 'Name', width: 130 },
+    { field: 'capital', headerName: 'Capital', width: 130 },
+    { field: 'region', headerName: 'Region', width: 130 },
+    { field: 'domains', headerName: 'Top Level Domains', width: 200 },
+    { field: 'borders', headerName: 'Borders', width: 200 },
+  ]
+  useEffect(() => {
+    const fetchCountryData = async () => {
+      const { data } = await axios.get('https://restcountries.eu/rest/v2/all')
+      const dataRequired = data.map((obj, idx) => ({
+        id: idx,
+        name: obj.name,
+        capital: obj.capital,
+        region: obj.region,
+        domains: obj.topLevelDomain.join(', '),
+        borders: obj.borders.join(', '),
+      }))
+      setAllCountryData(dataRequired)
+    }
 
-        fetchCountryData()
-    }, [])
+    fetchCountryData()
+  }, [])
 
-    useEffect(() => {
-        const rows = allCountryData.filter(
-            (obj, index) =>
-                !countryFilter ||
-                obj.name.toLowerCase().indexOf(countryFilter.toLowerCase()) > -1
-        )
-        setFilteredData(rows)
-    }, [allCountryData, countryFilter])
+  useEffect(() => {
+    if (!countryFilter) {
+      setFilteredData([...allCountryData])
+    } else {
+      const rows = allCountryData.filter((obj) => obj.name.toLowerCase().indexOf(countryFilter.toLowerCase()) > -1)
+      setFilteredData(rows)
+    }
+  }, [allCountryData, countryFilter])
 
-    return (
-        <div>
-            <div>
-                <span>Filter by Country:</span>
-                <input
-                    type="text"
-                    value={countryFilter}
-                    onChange={(e) => setCountryFilter(e.target.value)}
-                />
-            </div>
-            <br />
-            <div style={{ height: 700, width: '100%' }}>
-                <DataGrid rows={filteredData} columns={columns} pageSize={50} />
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div>
+        <span>Filter by Country:</span>
+        <input type="text" value={countryFilter} onChange={(e) => setCountryFilter(e.target.value)} />
+      </div>
+      <br />
+      <div style={{ height: 700, width: '100%' }}>
+        <DataGrid rows={filteredData} columns={columns} pageSize={50} />
+      </div>
+    </div>
+  )
 }
 
 export default CountryTable
